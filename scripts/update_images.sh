a7ae71274ea32ec7c9dd000287f007af9777ed1e
migraf <michael.graf3110@gmail.com>
2020-11-30 16:08:28 +0100

    updated master images with most common machine learning libraries

--- scripts/update_images.sh
+++ scripts/update_images.sh
@@ -2,9 +2,9 @@
 
 # TODO check build context
 docker login harbor.personalhealthtrain.de
-docker build -t harbor.personalhealthtrain.de/pht_master/master:dl -f ../docker/Dockerfile_dl ..
-docker build -t harbor.personalhealthtrain.de/pht_master/master:slim  -f ../docker/Dockerfile_slim ..
-docker build -t harbor.personalhealthtrain.de/pht_master/master:buster -f ../docker/Dockerfile_buster ..
+docker build -t harbor.personalhealthtrain.de/pht_master/master:dl -f ../docker_files/Dockerfile_dl ..
+docker build -t harbor.personalhealthtrain.de/pht_master/master:slim  -f ../docker_files/Dockerfile_slim ..
+docker build -t harbor.personalhealthtrain.de/pht_master/master:buster -f ../docker_files/Dockerfile_buster ..
 docker push harbor.personalhealthtrain.de/pht_master/master:dl
 docker push harbor.personalhealthtrain.de/pht_master/master:slim
 docker push harbor.personalhealthtrain.de/pht_master/master:buster
--- test/keygen.py
+++ test/keygen.py
@@ -39,14 +39,15 @@ if __name__ == '__main__':
     # generate_user_key_pair()
 
     # Generate simulated values for hashes
-    session_id = bytes.fromhex("da5b1de1025815cf4f2f7adfd6009ef5e516db6b15c2e895833b761ade7a3c1be0dd2e46aac70fb88f557963f628d9c2ece3e6597dcd50788cc9fde3d10b8a3b")
-    files = list()
-    print("parsing files")
-    for (dir_path, dir_names, file_names) in os.walk("../scripts"):
-        files += [os.path.join(dir_path, file) for file in file_names]
-    hash = hash_immutable_files(files, "1", session_id)
+    # session_id = bytes.fromhex("da5b1de1025815cf4f2f7adfd6009ef5e516db6b15c2e895833b761ade7a3c1be0dd2e46aac70fb88f557963f628d9c2ece3e6597dcd50788cc9fde3d10b8a3b")
+    # files = list()
+    # print("parsing files")
+    # for (dir_path, dir_names, file_names) in os.walk("../scripts"):
+    #     files += [os.path.join(dir_path, file) for file in file_names]
+    # # hash = hash_immutable_files(files, "1", session_id)
 
-    hash = bytes.fromhex("de0053cd0e55607bec5c99a5a5a10897b2c50f25d5a7c5f9191e91829d2e16e3ef2256d9f275cad5a88bc819812c8aabb4b4129c7d26ce8cac2f3f9b21438854")
+
+    hash = bytes.fromhex("b84e49ba9a09980af85f060d04d5dedac949620027005be786af9553a7d8967516f5de26b28dbd68b792c45fefb816ef17855d8e64fbe3a56f30eb821709da19")
     print("Hash: ", hash.hex())
     with open("../test/keys/user_private_key.pem", "rb") as pk:
         private_key = serialization.load_pem_private_key(pk.read(), password=None,
@@ -59,7 +60,7 @@ if __name__ == '__main__':
         print("Signature: ", sig.hex())
     with open("../test/keys/user_public_key.pem", "rb") as pk:
         pk_pem = pk.read().hex()
-        public_key: rsa.RSAPublicKey = serialization.load_pem_public_key(bytes.fromhex("2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d494942496a414e42676b71686b6947397730424151454641414f43415138414d49494243674b4341514541304d742f447737795059486e33527848722f48360a376e4b66636b466d4262447a316e2f7a6b735970696d656f727a326a49484f30317344613333526369734c4431483955645172524a76517878613654744d586b0a636f63324d514c344150614e3939582b39797562464150677854397853484e676d306a4c54746a4c524e6878515573654b75454f524e38366e696e354446504f0a44516371517171324b71434a7349526b656145636f37673838652b3945434f6e48412f37457577767956344a777730346d772f667449442f66446f43555330710a41506a6d2b544e5a4373764a706c376949556678424d546e47566656372f4c4b48554478637975446a64357966524d6c433776414837476a6e55527170464f4e0a4e654653444162696358386576313332366d38383057646e72364f6b796d532b4f6c4d562f4334636a6a47374435755a4d6539516f4b4d64325472324b6d64390a50514944415141420a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a"),
+        public_key: rsa.RSAPublicKey = serialization.load_pem_public_key(bytes.fromhex(pk_pem),
                                                                          backend=default_backend())
         print("Public Key:", pk_pem)
 
