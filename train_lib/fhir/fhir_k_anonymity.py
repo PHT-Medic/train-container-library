c789f77720225493178bd833ca721730464e9bbd
migraf <Eufnayg1!>
2021-07-21 18:43:05 +0200

    identifying columns in k-anon check and anonymization

--- train_lib/fhir/fhir_k_anonymity.py
+++ train_lib/fhir/fhir_k_anonymity.py
@@ -1,42 +1,45 @@
 from typing import List
 import pandas as pd
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
-from pandas.api.types import is_numeric_dtype, is_string_dtype
+from pandas.api.types import is_numeric_dtype, is_string_dtype, is_categorical_dtype
 from icecream import ic
 
 
-def is_k_anonymized(df: pd.DataFrame, k: int = 3):
+def is_k_anonymized(df: pd.DataFrame, k: int = 3, id_cols: List[str] = None):
     for index, row in df.iterrows():
-        query = ' & '.join([f'{col} == "{row[col]}"' for col in df.columns])
+        if id_cols:
+            query = ' & '.join([f'{col} == "{row[col]}"' for col in id_cols])
+        else:
+            query = ' & '.join([f'{col} == "{row[col]}"' for col in df.columns])
         rows = df.query(query)
         if rows.shape[0] < k:
             return False
     return True
 
 
-def anonymize(df: pd.DataFrame, k: int = 3, id_cols: List[str] = None, level: int = 2):
+def anonymize(df: pd.DataFrame, k: int = 3, id_cols: List[str] = None):
     anon_df = pd.DataFrame(columns=df.columns)
 
-    # TODO specify id columns
-    for col in df.columns:
+    # If id cols are given anonymize those otherwise use all columns
+    for col in id_cols if id_cols else df.columns:
         if is_datetime(df[col]):
             anon_df[col] = generalize_datetime_column(df[col])
 
         elif is_numeric_dtype(df[col]):
             anon_df[col] = generalize_numeric_column(df[col])
 
-        elif is_string_dtype(df[col]):
+        elif is_string_dtype(df[col]) or is_categorical_dtype(df[col]):
             # TODO categorical/string variable handling
             anon_df[col] = df[col]
 
     if is_k_anonymized(anon_df):
         return anon_df
 
-
     else:
         print("More generalization required")
 
-def generalize_numeric_column(num_col: pd.Series, level: int = 2):
+
+def generalize_numeric_column(num_col: pd.Series):
     return num_col
 
 
