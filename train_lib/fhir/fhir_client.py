c0071dcf3d453bc63b4396124b866a85caa89e8c
migraf <Eufnayg1!>
2021-08-17 14:01:58 +0200

    moved directory creation to dag

--- train_lib/fhir/fhir_client.py
+++ train_lib/fhir/fhir_client.py
@@ -230,9 +230,6 @@ class PHTFhirClient:
     def store_query_results(self, query_results, filename: str, storage_dir: str = None) -> str:
 
         storage_dir = storage_dir if storage_dir else os.getenv("TRAIN_DATA_DIR")
-        if not os.path.isdir(storage_dir):
-            os.mkdir(storage_dir)
-
         if not storage_dir:
             logger.warning("No storage directory specified, saving results to current working directory.")
             results_path = filename
