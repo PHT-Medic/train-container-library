3dbc5c574ba7a5f60aad17034e55156943a4a069
migraf <michael.graf3110@gmail.com>
2022-02-25 17:11:51 +0100

    clean up package and install requirements

--- scripts/update_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker login harbor.personalhealthtrain.de
-docker build -t harbor.personalhealthtrain.de/pht_master/master:dl -f ../docker_files/Dockerfile_dl ..
-docker build -t harbor.personalhealthtrain.de/pht_master/master:slim  -f ../docker_files/Dockerfile_slim ..
-docker build -t harbor.personalhealthtrain.de/pht_master/master:buster -f ../docker_files/Dockerfile_buster ..
-docker push harbor.personalhealthtrain.de/pht_master/master:dl
-docker push harbor.personalhealthtrain.de/pht_master/master:slim
-docker push harbor.personalhealthtrain.de/pht_master/master:buster
--- train_lib/train/__init__.py
+++
@@ -1,1 +0,0 @@
-from .build_test_train import build_test_train
