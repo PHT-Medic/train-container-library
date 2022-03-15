6d3c7f827a3706f9da6495f96f84dbc1308e0dfe
migraf <michael.graf3110@gmail.com>
2022-03-15 19:35:35 +0100

    patch version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pht-train-container-library",
-    version="1.1.0",
+    version="1.1.1",
     author="Michael Graf",
     author_email="michael.graf@uni-tuebingen.de",
     description="PHT train container library",
