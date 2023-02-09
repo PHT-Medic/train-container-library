2a5dfbf70803345c478d66401f16e0de385bb82c
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2023-02-09 13:32:53 +0100

    Replace station image with hello-world image for random image test

--- train_lib/tests/test_validate_master_image.py
+++ train_lib/tests/test_validate_master_image.py
@@ -11,12 +11,11 @@ def test_validate_master_image(master_image, train_image, docker_client):
     validate_train_image(master_image, train_image, docker_client=docker_client)
     print(f"{time.time() - start_t:.2f}s")
 
+    docker_client.images.pull("hello-world")
     start_t = time.time()
     print("\nComparison: master_image vs. random_image")
     with pytest.raises(ValueError):
-        validate_train_image(
-            master_image, "station_airflow", docker_client=docker_client
-        )
+        validate_train_image(master_image, "hello-world", docker_client=docker_client)
     print(f"{time.time() - start_t:.2f}s")
 
     start_t = time.time()
